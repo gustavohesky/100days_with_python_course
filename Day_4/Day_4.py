3028761505b228b928996ef09dbba8754b5c7d2d
Gustavo Hesky <hesky_909@hotmail.com>
2026-04-10 16:50:03 +0100

    Exercises Update

--- Day_4/Day_4.py
+++ Day_4/Day_4.py
@@ -2,20 +2,20 @@
 
 import random
 
-# random_integer = random.randint(1,10)
-# print(random_integer)
+random_integer = random.randint(1,10)
+print(random_integer)
 
-# random_number_0_to_1 = random.random() * 10
-# print(random_number_0_to_1)
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
 
-# random_float = random.uniform(1,10)
-# print(random_float)
+random_float = random.uniform(1,10)
+print(random_float)
 
-# heads_or_tails = random.randint(0,1)
-# if heads_or_tails == 1:
-#     print("Heads")
-# else:
-    # print("Tails")
+heads_or_tails = random.randint(0,1)
+if heads_or_tails == 1:
+    print("Heads")
+else:
+    print("Tails")
 
 ##Lists
 
