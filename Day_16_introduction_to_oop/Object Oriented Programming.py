7825593b4d4db6e967769903416783b2c54e402f
Gustavo Hesky <hesky_909@hotmail.com>
2026-04-27 14:15:35 +0100

    Day 16 - Introduction to OOP

---
+++ Day_16_introduction_to_oop/Object Oriented Programming.py
@@ -0,0 +1,28 @@
+# Object Oriented Programming 
+
+# car = CarBlueprint() #Car = Object and CarBlueprint() = Class - To differenciate the words are capital
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.right(90)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+# print(table)
+
